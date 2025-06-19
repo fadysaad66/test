5287fb3c80ae5fc4421affe1543c7ac3c87f1bfc
fady soliman <72667975+fadysaad66@users.noreply.github.com>
2025-06-19 20:09:12 +0400

    Create test_hivebox.py

---
+++ test_hivebox.py
@@ -0,0 +1,9 @@
+from unittest.mock import patch
+import hivebox
+
+def test_version_count():
+    with patch("builtins.print") as mock_print:
+        with patch("sys.exit") as mock_exit:
+            app.version_count()
+            mock_print.assert_called_with("The Current Version Of The App Is : V0.0.1")
+            mock_exit.assert_called_once()
