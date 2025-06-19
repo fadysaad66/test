cad1b04e1b28055b34bda3c8631f2d7a43a3bc15
fady soliman <72667975+fadysaad66@users.noreply.github.com>
2025-06-19 20:13:00 +0400

    Update test_hivebox.py

--- test_hivebox.py
+++ test_hivebox.py
@@ -4,6 +4,6 @@ import hivebox
 def test_version_count():
     with patch("builtins.print") as mock_print:
         with patch("sys.exit") as mock_exit:
-            app.version_count()
+            hivebox.version_count()
             mock_print.assert_called_with("The Current Version Of The App Is : V0.0.1")
             mock_exit.assert_called_once()
